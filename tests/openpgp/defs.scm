268e435f921a834909429063ea656186266a0f85
NIIBE Yutaka <gniibe@fsij.org>
2026-03-18 16:07:44 +0900

    tests:openpgp: With gpgtar, extract tarball into an empty directory.
    * tests/openpgp/defs.scm (setup-legacy-environment): Only
    call create-gpghome in case of no --unpack-tarball.
    
    --
    
    At the tarball creation by setup.scm, it calls create-gpghome.  So,
    it's not needed in case of --unpack-tarball.
    
    GnuPG-bug-id: 8159
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- tests/openpgp/defs.scm
+++ tests/openpgp/defs.scm
@@ -464,12 +464,12 @@
 ;; Initialize the test environment, install appropriate configuration
 ;; and start the agent, with the keys from the legacy test suite.
 (define (setup-legacy-environment)
-  (create-gpghome)
   (if (member "--unpack-tarball" *args*)
       (begin
 	(call-check `(,(tool 'gpgtar) --extract --directory=. ,(cadr *args*)))
 	(start-agent))
       (begin
+        (create-gpghome)
 	(start-agent)
 	(create-legacy-gpghome)))
   (preset-passphrases))
